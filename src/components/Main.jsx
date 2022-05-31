adf5092f43e72374deaec285965bbd124b918c54
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-31 17:07:50 +0300

    corrected spelling error

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,7 +12,7 @@ const Main = () => {
       setMovies(response.data.results);
     });
   }, []);
-  console.log(movie);
+  // console.log(movie);
 
   const truncateString = (str, num) => {
     if (str?.length > num) {
@@ -23,9 +23,9 @@ const Main = () => {
   }
 
   return (
-    <div className="w-full h=[550px] text-white">
+    <div className="w-full h-[550px] text-white">
       <div className="w-full h-full">
-        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+      <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
