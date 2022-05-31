0af179012f706923d13c02b7b8d0d7d564e3066e
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-31 12:49:35 +0300

    fitted bacdrop to cover all widths

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
   return (
     <div className="w-full h=[550px] text-white">
       <div className="w-full h-full">
-        <img src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
+        <img className="w-full h-full object-cover" src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
 
       </div>
     </div>
