2035e208a9356ee490a009cdeaf91507b7497697
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-31 15:04:35 +0300

    centered movie title on row section

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {FaHeart, FaRegHeart} from 'react-icons/fa'
 
 const Row = ({ title, fetchURL }) => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);  
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -17,7 +18,10 @@ const Row = ({ title, fetchURL }) => {
         <div id={"slider"}>
             {movies.map((item, id) => (
                 <div className="w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                    <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
+                    <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
+                    <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                        <p className='whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
+                    </div>
                 </div>
             ))}
         </div>
