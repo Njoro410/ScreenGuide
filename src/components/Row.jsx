1a09469c53c1d31aaa0a22d2279f48b8836afd38
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-31 16:17:21 +0300

    added navigation icons for desktop users

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import {MdChevronLeft, MdChevronRight} from "react-icons/md"
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -12,15 +13,20 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  
+
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4 ">{title}</h2>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center group">
+        <MdChevronLeft size={30} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" />
         <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
           {movies.map((item, id) => (
             <Movie key={id} item={item}/>
           ))}
         </div>
+        <MdChevronRight size={30} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" />
       </div>
     </>
   );
