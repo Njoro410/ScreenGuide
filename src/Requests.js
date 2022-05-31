de94eb6e072349df55b0928a5878c84e71cb05cd
njoro410 <brian.njoroge@student.moringaschool.com>
2022-05-31 17:07:26 +0300

    changed endpoints

--- src/Requests.js
+++ src/Requests.js
@@ -3,8 +3,8 @@ const key = '56fbaac7fd77013cc072d285a17ec005'
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/trending?api_key=${key}&language=en-US&page=1`,
-    requestHorror: `https://api.themoviedb.org/3/movie/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 }
 
