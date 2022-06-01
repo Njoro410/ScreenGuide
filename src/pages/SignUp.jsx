8c5f0c52370c57511e5e9b52956b08a981f01b38
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-01 15:57:24 +0300

    added sign up functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, signUp] = UserAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="w-full h-screen">
@@ -14,15 +29,38 @@ const SignUp = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
-              <form className="w-full flex flex-col py-4 ">
-                <input type="email" className="p-3 my-2 bg-gray-700 rounded  " placeholder="email" autoComplete="email" />
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4 "
+              >
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className="p-3 my-2 bg-gray-700 rounded  "
+                  placeholder="email"
+                  autoComplete="email"
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
                   type="password"
                   className="p-3 my-2 bg-gray-700 rounded"
                   placeholder="Password"
                   autoComplete="current-password"
                 />
-                <button className="bg-red-600 py-3 my-6 rounded font-bold ">Sign Up</button>
+                <button className="bg-red-600 py-3 my-6 rounded font-bold ">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember Me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-6">
+                  <span className="text-gray-600 mr-3 ">Have an account?</span>
+                  <Link to={"/login"}>Sign In</Link>
+                </p>
               </form>
             </div>
           </div>
