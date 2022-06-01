31e04472163cc931b7721d4333f74a97b8e1f5b1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-01 15:44:28 +0300

    added login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/970e664f-2df4-47ce-b4fa-446082f5abc1/7d1bedd2-5c26-416f-a7b6-ce18b2cf6b48/KE-en-20220523-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form className="w-full flex flex-col py-4 ">
+                <input
+                  type="email"
+                  className="p-3 my-2 bg-gray-700 rounded  "
+                  placeholder="email"
+                  autoComplete="email"
+                />
+                <input
+                  type="password"
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold ">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember Me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-6">
+                  <span className="text-gray-600 mr-3 ">Have an account?</span>
+                  <Link to={"/login"}>Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Login
