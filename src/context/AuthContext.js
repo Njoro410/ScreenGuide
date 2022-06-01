2c02e97e970233337e82eb831f5cd42248769ac7
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-01 13:59:58 +0300

    added a user state check

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,6 +14,25 @@ export function AuthContextProvider({ children }) {
         return createUserWithEmailAndPassword(auth,email,password)
     }
 
+    function logIn(email,password) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logOut() {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=> {
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    })
+
+
+
     return (
         <AuthContext.Provider value={{signUp, user}}>
             {children}
