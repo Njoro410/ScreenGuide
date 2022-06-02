5441f9a587102e9d56b6027898e056dc8b50af2b
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-02 09:10:51 +0300

    added firestore db

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,17 +2,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SEND,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyDXOijp_OrGVo55xh6YXG2t_93xVANJi7I",
+  authDomain: "netflix-guide-2c8cc.firebaseapp.com",
+  projectId: "netflix-guide-2c8cc",
+  storageBucket: "netflix-guide-2c8cc.appspot.com",
+  messagingSenderId: "117581114055",
+  appId: "1:117581114055:web:dec68cb6cbc9623c156ce2"
 };
 
 
@@ -20,3 +22,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
