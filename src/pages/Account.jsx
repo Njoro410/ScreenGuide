4190ee63fd367ea77089d9d14367450f00d3a1f1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-02 09:56:39 +0300

    added mymovies saved page

---
+++ src/pages/Account.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/970e664f-2df4-47ce-b4fa-446082f5abc1/7d1bedd2-5c26-416f-a7b6-ce18b2cf6b48/KE-en-20220523-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">My Movies</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Account;
