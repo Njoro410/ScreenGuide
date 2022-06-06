4f06521ad9ab39d1031775e5ed87b62ad9915678
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-06 13:52:40 +0300

    added extra routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Trailers from "./components/Trailers";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
+import Details from "./pages/Details";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
+          <Route path="/trailer" element={<Trailers />} />
+          <Route path="/details/:id" element={<Details/>} />
           <Route path="/account" element={
           <ProtectedRoute>
             <Account />
