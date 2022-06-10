92d9d49fa32454ae9b575527f1a5dc5ddb521b9e
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-10 11:29:00 +0300

    added movie details

--- src/components/DetailsView.jsx
+++ src/components/DetailsView.jsx
@@ -1,15 +1,85 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailsView = () => {
-    const {id} = useParams()
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=56fbaac7fd77013cc072d285a17ec005&language=en-US`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setDetails(response.data);
+      });
+  }, []);
+
+//  const timeConvert = (n) => {
+//   var num = n;
+//   var hours = (num / 60);
+//   var rhours = Math.floor(hours);
+//   var minutes = (hours - rhours) * 60;
+//   var rminutes = Math.round(minutes);
+//   return rhours + " h : " + rminutes + " m";
+//  }
   return (
-    <div className="absolute w-full top-[20%] p-4 md:p-8">
-        <h1 className="text-3xl mt-10 text-white md:text-5xl font-bold">{id}</h1>
-
+    <div className="w-full h-[650px] text-white">
+      <div className="w-full h-full">
+        <div className="bg-black/90 fixed top-0 left-0 w-full h-screen"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${details?.backdrop_path}`}
+          alt={details?.title}
+        />
+        <div className="absolute w-full top-[10%] p-4 md:p-8">
+          <div className="grid md:grid-cols-3  gap-0 ">
+            <div className="md:col-span-1 ">
+              <img
+                className="h-[80%] rounded"
+                src={`https://image.tmdb.org/t/p/w500/${details?.poster_path}`}
+                alt={details?.poster_path}
+              />
+            </div>
+            <div className="md:col-span-2">
+              <p className="text-3xl text-red-400 md:text-5xl font-bold mt-5">
+                {details?.title}({details?.release_date?.substring(0, 4)})
+              </p>
+              <p className="text-gray-400 text-lg italic">{details?.tagline}</p>
+              <div className="flex flex-row gap-2">
+                {details.genres?.map((item, index) => (
+                  <div key={index}>
+                    <p>•{item.name}•</p>
+                  </div>
+                ))}
+              </div>
+              <div className="mt-5">
+                <p>
+                  {details.production_countries?.map((item, index) => (
+                    <div key={index}>
+                      <p className="text-gray-400 text-lg">
+                        {details?.status}•{details?.release_date}•(
+                        {item.name})
+                      </p>
+                    </div>
+                  ))}
+                </p>
+              </div>
+              <div>
+                <p className="text-3xl  font-bold my-2 ">Overview</p>
+                <p className="">
+                  {details?.overview}
+                </p>
+                <p>{timeConvert(details?.runtime) }</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsView
+export default DetailsView;
