a3b3b48d76c0f076cc397c22469c4a1122b0586e
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-11 12:38:18 +0300

    fixed a bug when passing prop to child component

--- src/components/DetailsView.jsx
+++ src/components/DetailsView.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Cast from "./Cast";
 
 const DetailsView = () => {
   const { id } = useParams();
-  const [details, setDetails] = useState([]);
+  const [details, setDetails] = useState({});
 
   useEffect(() => {
     axios
@@ -12,19 +13,18 @@ const DetailsView = () => {
         `https://api.themoviedb.org/3/movie/${id}?api_key=56fbaac7fd77013cc072d285a17ec005&language=en-US`
       )
       .then((response) => {
-        console.log(response.data);
         setDetails(response.data);
       });
   }, []);
 
- const timeConvert = (n) => {
-  var num = n;
-  var hours = (num / 60);
-  var rhours = Math.floor(hours);
-  var minutes = (hours - rhours) * 60;
-  var rminutes = Math.round(minutes);
-  return rhours + " h : " + rminutes + " m";
- }
+  const timeConvert = (n) => {
+    var hours = n / 60;
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return rhours + " h : " + rminutes + " m";
+  };
+  
   return (
     <div className="w-full h-[650px] text-white">
       <div className="w-full h-full">
@@ -44,16 +44,23 @@ const DetailsView = () => {
               />
             </div>
             <div className="md:col-span-2">
-              <p className="text-3xl text-red-400 md:text-5xl font-bold mt-5">
-                {details?.title}({details?.release_date?.substring(0, 4)})
-              </p>
+              <a href={details?.homepage}>
+                <p className="text-3xl text-red-400 md:text-5xl font-bold mt-5">
+                  {details?.title}({details?.release_date?.substring(0, 4)})
+                </p>
+              </a>
               <p className="text-gray-400 text-lg italic">{details?.tagline}</p>
               <div className="flex flex-row gap-2">
                 {details.genres?.map((item, index) => (
                   <div key={index}>
-                    <p>•{item.name}•</p>
+                    <p className="bg-gray-600 px-2 py-0.5 rounded">
+                      {item.name}
+                    </p>
                   </div>
-                ))}
+                ))}{" "}
+                <p className="bg-gray-600 px-2 py-0.5 rounded">
+                  {timeConvert(details?.runtime)}
+                </p>
               </div>
               <div className="mt-5">
                 <p>
@@ -69,16 +76,16 @@ const DetailsView = () => {
               </div>
               <div>
                 <p className="text-3xl  font-bold my-2 ">Overview</p>
-                <p className="">
-                  {details?.overview}
-                </p>
-                <p>{timeConvert(details?.runtime) }</p>
+                <p className="">{details?.overview}</p>
               </div>
             </div>
           </div>
         </div>
+        {details?.id && <Cast id={details?.id}/>}
       </div>
+      
     </div>
+    
   );
 };
 
