eba1382d3ecb77caa8e1d05069ceafc4876177df
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-13 09:50:15 +0300

    displayed movie poaster if backdrop not available

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,42 +1,51 @@
 import React, { useState } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
-import {UserAuth} from "../context/AuthContext"
-import {db} from "../Firebase"
-import {arrayUnion, doc, updateDoc} from "firebase/firestore"
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../Firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
-const Movie = ({item}) => {
-    const [like, setLike] = useState(false);
-    const [saved, setSaved] = useState(false);
-    const {user} = UserAuth();
+const Movie = ({ item }) => {
+  const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = UserAuth();
 
-    const movieID = doc(db, "users", `${user?.email}`)
+  const movieID = doc(db, "users", `${user?.email}`);
 
-    const saveMovie = async () => {
-      if(user?.email) {
-        setLike(!like)
-        setSaved(true)
-        await updateDoc(movieID,{
-          savedShows: arrayUnion({
-            id: item.id, 
-            title: item.title,
-            img: item.backdrop_path
-          })
-        })
-      }else {
-        alert('Log in to save a movie')
-      }
+  const saveMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: item.id,
+          title: item.title,
+          img: item.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Log in to save a movie");
     }
+  };
 
   return (
     <div className="w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-      <img
+      {item?.backdrop_path ? (
+        <img
         className="w-full h-auto block"
-        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path }`}
         alt={item?.title}
       />
+      ):(
+        <img
+        className="w-full h-[150px] block"
+        src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
+        alt={item?.title}/>
+      )}
+    
+
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
         <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-          {item?.title}
+          {item?.title || item?.original_name}
         </p>
         <p onClick={saveMovie}>
           {like ? (
