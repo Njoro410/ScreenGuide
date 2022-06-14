1f5da57a8064a730370406030cdb18a7885a7bd9
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-14 11:53:18 +0300

    added details view page

---
+++ src/pages/Details.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Cast from "../components/Cast";
+import DetailsView from "../components/DetailsView";
+
+const Details = () => {
+  return (
+    <>
+      <DetailsView />
+    </>
+  );
+};
+
+export default Details;
