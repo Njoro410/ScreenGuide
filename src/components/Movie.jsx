c069e281c26402265a3857eafb9fd05c6537f3b4
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-14 11:49:41 +0300

    added sweetalert error popup

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,6 +3,8 @@ import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../Firebase";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
 
 const Movie = ({ item }) => {
   const [like, setLike] = useState(false);
@@ -23,30 +25,62 @@ const Movie = ({ item }) => {
         }),
       });
     } else {
-      alert("Log in to save a movie");
+      Swal.fire({
+        title: "Error!",
+        text: "Log in to save a movie",
+        icon: "error",
+      });
+      // await Toast.fire({
+      //   icon: 'success',
+      //   title: item?.title || item?.original_name
+      // })
     }
   };
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-right',
+    iconColor: 'green',
+    customClass: {
+      popup: 'colored-toast'
+    },
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true
+  })
+
 
   return (
     <div className="w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       {item?.backdrop_path ? (
         <img
-        className="w-full h-auto block"
-        src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path }`}
-        alt={item?.title}
-      />
-      ):(
+          className="w-full h-auto block"
+          src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+          alt={item?.title}
+        />
+      ) : (
         <img
-        className="w-full h-[150px] block"
-        src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
-        alt={item?.title}/>
+          className="w-full h-[150px] block"
+          src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
+          alt={item?.title}
+        />
       )}
-    
 
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-        <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-          {item?.title || item?.original_name}
-        </p>
+        {item?.first_air_date ? (
+          <Link to={`/series/${item?.id} `}>
+            <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+              {item?.original_name}
+            </p>
+          </Link>
+        ) : (
+          <Link to={`/details/${item?.id} `}>
+            <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+              {item?.title}
+            </p>
+          </Link>
+        )}
+
         <p onClick={saveMovie}>
           {like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
