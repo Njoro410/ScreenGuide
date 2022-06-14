aac6f854ba45b4f7842bc1b3d2c551a0ea469838
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-14 11:54:19 +0300

    added series routes

---
+++ src/pages/TVShows.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Series from "../components/Series";
+import requests from "../Requests";
+import Row from '../components/Row'
+
+const TVShows = () => {
+  return (
+    <>
+      <Series />
+      <Row rowID='1' title='On The Air' fetchURL = {requests.requestUpcomingShows}/>
+      <Row rowID='2' title='Popular' fetchURL = {requests.requestPopularShows}/>
+      <Row rowID='3' title='Trending' fetchURL = {requests.requestTrendingShows}/>
+      <Row rowID='4' title='Top Rated' fetchURL = {requests.requestTopRatedShows}/>
+      <Row rowID='5' title='Horror' fetchURL = {requests.requestHorrorShows}/>
+      {/* <Row rowID='6' title='Airing Today' fetchURL = {requests.requestAiringToday}/> */}
+    </>
+  );
+};
+
+export default TVShows;
