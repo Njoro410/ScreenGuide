25e9970c1396cbc1dd67e6b585756b385d071fec
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-14 11:51:34 +0300

    modified linking to prevent routing in movie save

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import {MdChevronLeft, MdChevronRight} from "react-icons/md"
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
-
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -13,27 +13,46 @@ const Row = ({ title, fetchURL, rowID }) => {
     });
   }, [fetchURL]);
 
-  const slideLeft = ()=> {
-    var slider = document.getElementById('slider' + rowID)
-    slider.scrollLeft = slider.scrollLeft - 500
-  }
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
 
-  const slideRight = ()=> {
-    var slider = document.getElementById('slider' + rowID)
-    slider.scrollLeft = slider.scrollLeft + 500
-  }
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4 ">{title}</h2>
       <div className="relative flex items-center group">
-        <MdChevronLeft onClick={slideLeft} size={30} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" />
-        <div id={"slider" + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
-          {movies.map((item, id) => (
-            <Movie key={id} item={item}/>
-          ))}
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={30}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies.map((item, id) =>
+            item?.first_air_date ? (
+              // <Link to={`/series/${item?.id} `}>
+                <Movie key={id} item={item} />
+              // </Link>
+            ) : (
+              // <Link to={`/details/${item?.id} `}>
+                <Movie key={id} item={item} />
+              // </Link>
+            )
+          )}
         </div>
-        <MdChevronRight onClick={slideRight} size={30} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block" />
+        <MdChevronRight
+          onClick={slideRight}
+          size={30}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
       </div>
     </>
   );
