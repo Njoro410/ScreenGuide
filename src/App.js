33af356ce18928339f637633b863dcc7eeb95d37
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-16 12:20:14 +0300

    removed trailer path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Trailers from "./components/Trailers";
+import SeriesDetails from "./components/SeriesDetails";
 import { AuthContextProvider } from "./context/AuthContext";
 import Account from "./pages/Account";
 import Details from "./pages/Details";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import TVShows from "./pages/TVShows";
 
 function App() {
   return (
@@ -18,8 +19,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/trailer" element={<Trailers />} />
           <Route path="/details/:id" element={<Details/>} />
+          <Route path="/series/:id" element={<SeriesDetails />} />
+          <Route path="/series" element={<TVShows/>} />
           <Route path="/account" element={
           <ProtectedRoute>
             <Account />
