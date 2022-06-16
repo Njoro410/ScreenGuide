aa28a02c19200408d415ce5b3b26ffe41132b3ff
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-16 12:17:57 +0300

    modal add on main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import requests from "../Requests";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Modal from "./Modal";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [open, setOpen] = useState(false);
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
@@ -23,10 +25,18 @@ const Main = () => {
     }
   };
 
+
   return (
     <div className="w-full h-[550px] text-white">
+      {open && (
+        <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
+          {movie?.id && <Modal name={movie?.title} id={movie?.id} closeModal={setOpen} />}
+        </div>
+      )}
+
       <div className="w-full h-full">
         <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
@@ -35,15 +45,20 @@ const Main = () => {
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="my-4">
-            <Link to={"/trailer"}>
-              <button className="border bg-gray-200 text-black border-gray-300 py-2 px-5">
-                Play Trailer
-              </button>
-            </Link>
+            <a
+              onClick={() => {
+                setOpen(true);
+              }}
+              type="button"
+              className="border bg-gray-200 text-black border-gray-300 py-2 px-5"
+            >
+              Play Trailer
+            </a>
+
             <Link to={`/details/${movie?.id} `}>
-            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
-              See More
-            </button>
+              <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+                See More
+              </button>
             </Link>
           </div>
           <p className="text-gray-400 text-sm">
