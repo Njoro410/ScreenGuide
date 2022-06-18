038bf8ad335e9d7640f1745272dcde469a325ace
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-18 15:12:09 +0300

    added toasts and alerts for better user interaction

--- src/components/DetailsView.jsx
+++ src/components/DetailsView.jsx
@@ -5,11 +5,82 @@ import Cast from "./Cast";
 import Modal from "./Modal";
 import { FaHeart, FaRegHeart, FaEye, FaCheck } from "react-icons/fa";
 import ReactTooltip from "react-tooltip";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../Firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import Swal from "sweetalert2";
 
 const DetailsView = () => {
   const { id } = useParams();
   const [details, setDetails] = useState({});
   const [open, setOpen] = useState(false);
+  const { user } = UserAuth();
+  const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+
+  const movieID = doc(db, "users", `${user?.email}`);
+
+  const saveMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: details.id,
+          title: details.title,
+          img: details.backdrop_path,
+        }),
+      });
+      await Toast.fire({
+        icon: "success",
+        title: details?.title,
+        text: "Marked as favourite"
+      });
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Log in to save a movie",
+        icon: "error",
+      });
+    }
+  };
+
+  const watchedMovie = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        watchedShows: arrayUnion({
+          id: details.id,
+          title: details.title,
+          img: details.backdrop_path,
+        }),
+      });
+      await Toast.fire({
+        icon: "success",
+        title: details?.title,
+        text: "set as watched"
+      });
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Log in to save a movie",
+        icon: "error",
+      });
+    }
+  };
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-right",
+    iconColor: "green",
+    customClass: {
+      popup: "colored-toast",
+    },
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+  });
 
   useEffect(() => {
     axios
@@ -35,7 +106,7 @@ const DetailsView = () => {
   };
 
   return (
-    <div className="md:w-full h-[580px] text-white">
+    <div className="md:w-full h-[650px] text-white">
       <div className="w-full h-full">
         <div className="bg-black/90 fixed top-0 left-0 w-full h-screen"></div>
         <img
@@ -47,14 +118,14 @@ const DetailsView = () => {
           <div className="grid md:grid-cols-3  gap-0 ">
             <div className="flex justify-center md:col-span-1">
               <img
-                className="h-[70%] rounded"
+                className="h-[80%] rounded"
                 src={`https://image.tmdb.org/t/p/w500/${details?.poster_path}`}
                 alt={details?.poster_path}
               />
             </div>
             <div className="md:col-span-2 ">
               <a href={details?.homepage}>
-                <p className="text-3xl text-red-400 md:text-5xl font-bold mt-12">
+                <p className="text-3xl text-green-400 md:text-5xl font-bold mt-12">
                   {details?.title}({details?.release_date?.substring(0, 4)})
                 </p>
               </a>
@@ -63,7 +134,7 @@ const DetailsView = () => {
                 {details.genres?.map((item, index) => (
                   <div key={index}>
                     <p className="bg-gray-600 px-2 py-0.5 rounded">
-                      {item.name}
+                      {item?.name}
                     </p>
                   </div>
                 ))}{" "}
@@ -82,16 +153,32 @@ const DetailsView = () => {
                 ))}
               </div>
               <div>
-                <p className="text-3xl  font-bold my-2 ">Overview</p>
+                <p className="text-3xl  font-bold my-2 text-green-400">Overview</p>
                 <p className="">{details?.overview}</p>
               </div>
+              {/* <div>
+                <p className="text-3xl  font-bold my-2 text-green-400">
+                  Networks
+                </p>
+                
+                {details.production_companies?.map((item, index) => (
+                  
+                  <div key={index} className="inline-block mx-2">
+                    <img
+                      className="h-8 rounded bg-green-400/20"
+                      src={`https://image.tmdb.org/t/p/w500/${item.logo_path}`}
+                      alt={item?.logo_path}
+                    />
+                  </div>
+                ))}{" "}
+              </div> */}
               <div className="flex gap-5">
-                <div className="mt-5 font-bold bg-slate-900 p-3 rounded-full w-32 text-center">
+                <div className="mt-5 font-bold bg-green-900 p-3 rounded-full w-32 text-center">
                   <p>{percent(details?.vote_average)}%-Like this</p>
                 </div>
                 <div className="mt-5 ">
                   <button
-                    className="font-bold bg-slate-900 p-3 rounded-full w-32 text-center hover:bg-slate-700"
+                    className="font-bold bg-green-900 p-3 rounded-full w-32 text-center hover:bg-green-700"
                     onClick={() => {
                       setOpen(true);
                     }}
@@ -113,16 +200,16 @@ const DetailsView = () => {
                 </div>
                 <div data-tip="Favourite" className="mt-5">
                   <ReactTooltip type="info" effect="float" />
-                  <button
-                    className="font-bold bg-slate-900 p-4 rounded-full w-32 flex justify-center  hover:bg-slate-700"
+                  <button onClick={saveMovie}
+                    className="font-bold bg-green-900 p-4 rounded-full w-32 flex justify-center  hover:bg-green-700"
                     type="button"
                   >
                     <FaHeart />
                   </button>
                 </div>
                 <div data-tip="Watched" className="mt-5">
-                  <button
-                    className="font-bold bg-slate-900 p-4 rounded-full w-32 flex justify-center  hover:bg-slate-700"
+                  <button onClick={watchedMovie}
+                    className="font-bold bg-green-900 p-4 rounded-full w-32 flex justify-center  hover:bg-green-700"
                     type="button"
                   >
                     <FaCheck />
