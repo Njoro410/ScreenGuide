7d52f48adeb9c08beddbd83af8ce0d1fe88453db
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-18 15:26:22 +0300

    removed console log for user details

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -105,7 +105,7 @@ export function AuthContextProvider({ children }) {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
-            console.log("user", currentUser)
+           
 
         })
         return () => {
