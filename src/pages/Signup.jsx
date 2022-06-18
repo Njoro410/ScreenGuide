e161cd76fa49c9c2420b5c1cb0f21c67f3e0d5f4
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-18 15:03:59 +0300

    added sign up via facebook

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ import { GoogleButton } from "react-google-button";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { user, signUp, googleSignIn } = UserAuth();
+  const { user, signUp, googleSignIn, facebookSignIn } = UserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -22,7 +22,14 @@ const SignUp = () => {
   const handlegoogleSignIn = async () => {
     try {
       await googleSignIn();
-    
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlefacebookSignIn = async () => {
+    try {
+      await facebookSignIn();
     } catch (error) {
       console.log(error);
     }
@@ -44,31 +51,90 @@ const SignUp = () => {
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
         <div className="fixed w-full px-4 py-24 z-50">
-          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[450px] h-[500px] mx-auto bg-black/75 text-green-400">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
               <form
                 onSubmit={handleSubmit}
                 className="w-full flex flex-col py-4 "
               >
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  className="p-3 my-2 bg-gray-700 rounded  "
-                  placeholder="email"
-                  autoComplete="email"
-                />
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  className="p-3 my-2 bg-gray-700 rounded"
-                  placeholder="Password"
-                  autoComplete="current-password"
-                />
-                <button className="bg-red-600 py-3 my-6 rounded font-bold ">
+                <div className="relative">
+                  <input
+                    type="email"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="block p-3 my-2 rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-300 bg-gray-700 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-green-400 peer"
+                    placeholder=" "
+                  />
+                  <label
+                    for="email"
+                    className="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-green-400 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+                  >
+                    Email
+                  </label>
+                </div>
+
+                <div className="relative">
+                  <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="block my-2 p-3 rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-300 bg-gray-700 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-green-400 peer"
+                    placeholder=" "
+                  />
+                  <label
+                    for="password"
+                    className="absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-green-400 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+                  >
+                    Password
+                  </label>
+                </div>
+                <button className="bg-green-400 hover:bg-green-600 py-3 my-1 rounded font-bold text-white">
                   Sign Up
                 </button>
-                <GoogleButton onClick={handlegoogleSignIn} />
+                <button
+                  onClick={handlegoogleSignIn}
+                  type="button"
+                  className="text-white bg-[#4285F4] hover:bg-[#4285F4]/70 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded text-sm px-5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 py-3 my-1"
+                >
+                  <svg
+                    className="w-4 h-4 mr-2 -ml-1"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="google"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 488 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                    ></path>
+                  </svg>
+                  Sign in with Google
+                </button>
+                <button onClick={handlefacebookSignIn}
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-700/70 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded text-sm px-5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 py-3 my-1"
+                >
+                  <svg
+                    className="w-4 h-4 mr-2 -ml-1"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fab"
+                    data-icon="facebook-f"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 320 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+                    ></path>
+                  </svg>
+                  Sign in with Facebook
+                </button>
                 <div className="flex justify-between items-center text-sm text-gray-600">
                   <p>
                     <input className="mr-2" type="checkbox" />
