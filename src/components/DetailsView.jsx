a0a2a0f81f54b2a47f42e45c0aee7cc16c0cfdef
njoro410 <brian.njoroge@student.moringaschool.com>
2022-06-18 15:28:16 +0300

    removed await fxn for toast

--- src/components/DetailsView.jsx
+++ src/components/DetailsView.jsx
@@ -31,7 +31,7 @@ const DetailsView = () => {
           img: details.backdrop_path,
         }),
       });
-      await Toast.fire({
+       Toast.fire({
         icon: "success",
         title: details?.title,
         text: "Marked as favourite"
@@ -56,7 +56,7 @@ const DetailsView = () => {
           img: details.backdrop_path,
         }),
       });
-      await Toast.fire({
+       Toast.fire({
         icon: "success",
         title: details?.title,
         text: "set as watched"
